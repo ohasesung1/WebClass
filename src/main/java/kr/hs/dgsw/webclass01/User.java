1b1663a31dca1a95cac0188c8c68c075e9b24fa8
ohasesung1 <40197864+ohasesung1@users.noreply.github.com>
2020-05-08 12:20:33 +0900

    [UPDATE] lombok, comment
    주석 추가 과제, 람북 적용 과제

---
+++ src/main/java/kr/hs/dgsw/webclass01/User.java
@@ -0,0 +1,48 @@
+package kr.hs.dgsw.webclass01;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class User {
+  private @NonNull String id;
+  private @NonNull String name;
+  private @NonNull String email;
+
+  @Override
+  public String toString() {
+    return id + "/" + name + "/" + email;
+  }
+
+  public User() {}
+
+  public User(String id, String name, String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  // public String getId() {
+  //   return id;
+  // }
+
+  // public void setId(String id) {
+  //   this.id = id;
+  // }
+
+  // public String getName() {
+  //   return name;
+  // }
+
+  // public void setName(String name) {
+  //   this.name = name;
+  // }
+
+  // public String getEmail() {
+  //   return email;
+  // }
+
+  // public void setEmail(String email) {
+  //   this.email = email;
+  // }
+}
